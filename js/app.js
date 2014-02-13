dbadf40f1d614d974fc96a591ed29ec70f15784f
Aisha Green <skyfaerie22@gmail.com>
2014-02-13 17:13:44 -0500

    added masonry

--- js/app.js
+++ js/app.js
@@ -1,3 +1,18 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
+
+$(function(){
+  $('#mentors').masonry({
+    columnWidth: 25,
+    gutter: 40,
+    itemSelector: '.mentor'
+  });
+
+  $('#partners').masonry({
+    columnWidth: 33,
+    gutter: 40,
+    itemSelector: '.partner'
+  });
+});
+
