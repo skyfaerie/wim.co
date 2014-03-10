40bd6f1324b1d4f2bb780e070faf5e68cb74da29
Aisha Green <skyfaerie22@gmail.com>
2014-03-10 13:14:34 -0400

    parallax for the homepage

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
 
-$(function(){
+$(function() {
   $('#mentors').masonry({
     "isFitWidth": true,
     itemSelector: '.mentor'
@@ -14,3 +14,54 @@ $(function(){
   });
 });
 
+//Parallax
+
+jQuery(document).ready(function($) {
+
+  $(window).stellar();
+
+  var links = $('.navigation').find('li');
+  slide = $('.slide');
+  button = $('.button');
+  mywindow = $(window);
+  htmlbody = $('html,body');
+
+
+  slide.waypoint(function(event, direction) {
+
+    dataslide = $(this).attr('data-slide');
+
+    if (direction === 'down') {
+      $('.navigation li[data-slide="' + dataslide + '"]').addClass('active').prev().removeClass('active');
+    } else {
+      $('.navigation li[data-slide="' + dataslide + '"]').addClass('active').next().removeClass('active');
+    }
+
+  });
+
+  mywindow.scroll(function() {
+    if (mywindow.scrollTop() === 0) {
+      $('.navigation li[data-slide="1"]').addClass('active');
+      $('.navigation li[data-slide="2"]').removeClass('active');
+    }
+  });
+
+  function goToByScroll(dataslide) {
+    htmlbody.animate({
+      scrollTop: $('.slide[data-slide="' + dataslide + '"]').offset().top
+    }, 2000, 'easeInOutQuint');
+  }
+
+  links.click(function(e) {
+    e.preventDefault();
+    dataslide = $(this).attr('data-slide');
+    goToByScroll(dataslide);
+  });
+
+  button.click(function(e) {
+    e.preventDefault();
+    dataslide = $(this).attr('data-slide');
+    goToByScroll(dataslide);
+
+  });
+});
